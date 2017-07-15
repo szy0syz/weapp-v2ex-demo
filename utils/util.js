2184831635eb30ba01467165b1d883c3399acfd7
szy0syz <szy0syz@gmail.com>
2017-07-15 23:53:33 +0800

    updated util.js

--- utils/util.js
+++ utils/util.js
@@ -1,26 +1,7 @@
-function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
-
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
 function formatUTC(num) {
   return new Date(num * 1000).toLocaleString()
 }
 
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
-
 module.exports = {
-  formatTime: formatTime,
   formatUTC: formatUTC
 }
